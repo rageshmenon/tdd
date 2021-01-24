da266bdb52b7af63b8720ab5e21dbb14223b8136
ravindran.ragesh <ravindran.ragesh@digital.homeoffice.gov.uk>
2021-01-24 21:05:53 +0000

    Initial Commit

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,17 @@
 package co.interleap.courses.tdd;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class VowelCounter {
+
+    private final List<String> VOWEL = Arrays.asList("a", "e", "i", "o", "u");
+
+    public long count(String s) {
+
+        return Arrays.stream(s.split(""))
+                .filter(VOWEL::contains)
+                .count();
+
+    }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,30 @@
 package co.interleap.courses.tdd;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void testVowelCountWithEmptyString() {
+        VowelCounter vowelCounter = new VowelCounter();
+
+        assertEquals(0, vowelCounter.count(""));
+    }
+
+    @Test
+    public void testVowelCountWithOnly1VowelString() {
+        VowelCounter vowelCounter = new VowelCounter();
+
+        assertEquals(1, vowelCounter.count("a"));
+    }
+
+    @Test
+    public void testVowelCountWithManyVowelString() {
+        VowelCounter vowelCounter = new VowelCounter();
+
+        assertEquals(3, vowelCounter.count("aei"));
+    }
 }
